0aa411e00535090218f7396b53b5bc0d16afc711
Dohyung Ahn <dohyung.ahn@navercorp.com>
2026-09-04 08:20:56 +0900

    feat(herdr): add herdr-nvim and enable custom.plugins import
    herdr-nvim의 nvim 절반(코드 주석을 에이전트에 전송)을 추가한다.
    herdr 쪽 플러그인과 키바인딩(prefix+e, prefix+o)은 이미 설정되어 있었다.
    
    custom.plugins import가 주석 처리되어 있어서 lua/custom/plugins/*.lua를
    lazy가 읽지 않는 상태였다. 함께 활성화한다.

---
+++ lua/custom/plugins/herdr.lua
@@ -0,0 +1,6 @@
+-- herdr-nvim: code annotations you send to the agent in your herdr workspace.
+-- Keymaps default to the `<leader>a` prefix (ac/al/as/aS).
+return {
+  'ChmaraX/herdr-nvim',
+  opts = {},
+}
